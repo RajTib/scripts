3677f6cb4d255a333b2c4e7ff465eda14369c57f
Raj Tibarewala <raj.tibarewala@gmail.com>
2026-09-07 23:32:25 +0530

    mavros

---
+++ start_mavros.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /opt/ros/humble/setup.bash
+source ~/ros2_ws/install/setup.bash
+
+echo "Starting MAVROS..."
+echo "Pixhawk: 192.168.144.14:14550"
+
+ros2 launch mavros.apm.launch \
+    fcu_url:=udp://@192.168.144.14:14550
